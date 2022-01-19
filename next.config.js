452a5a83e147329b6189eb4d9643094b569ea700
Escalion <escalion86@gmail.com>
2022-01-19 14:55:33 +0700

    Init

---
+++ next.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+	// images: {
+	// 	domains: ['courses-top.ru']
+	// },
+	webpack(config, options) {
+		config.module.rules.push({
+			// test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      // use: ['@svgr/webpack'],
+			loader: '@svgr/webpack',
+			options: {
+				prettier: false,
+				svgo: true,
+				svgoConfig: {
+					plugins: [{ 
+						name: 'preset-default',
+						params: {
+							override: {
+								removeViewBox: false
+							}
+						}
+					}],
+				},
+				titleProp: true,
+			},
+			test: /\.svg$/,
+		});
+
+		return config;
+	},
+};
