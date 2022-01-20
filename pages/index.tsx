e82f2bc8bb210b6427c3153162627bd58bd1c875
Escalion <escalion86@gmail.com>
2022-01-21 04:09:48 +0700

    Chapter 12 + 13.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
+import { GetStaticProps } from 'next'
 import { useState } from 'react'
 import { Button, Htag, P, Rating, Tag  } from '../components'
 import { Layout, withLayout } from '../layout/Layout'
+import axios from 'axios'
+import { MenuItem } from '../interfaces/menu.interface'
 
-function Home(): JSX.Element {
+function Home({menu} : HomeProps): JSX.Element {
   const [rating,setRating] = useState<number>(4)
   return (
     <>
@@ -24,8 +27,27 @@ function Home(): JSX.Element {
       <Tag size='m' color='red'>Red</Tag>
       <Tag size='m' color='green'>Green</Tag>
       <Rating isEditable rating={rating} setRating={setRating} />
+      
     </>
   )
 }
 
 export default withLayout(Home)
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0
+  const { data: menu }  = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  })
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  }
+}
+
+interface HomeProps extends Record<string, unknown>{
+  menu: MenuItem[],
+  firstCategory: number
+}
